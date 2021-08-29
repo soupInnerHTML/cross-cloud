6bbbb1833528bf6028ae7daefe644476b46a0b52
soupInnerHTML <andrey.da.scrim@gmail.com>
2021-08-29 12:26:19 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Auth from './Auth';
+
+const App = () => {
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <Auth />
+        </SafeAreaView>
+    );
+};
+
+export default App;
